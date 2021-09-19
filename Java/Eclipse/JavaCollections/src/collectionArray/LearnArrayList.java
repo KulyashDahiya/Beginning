38d498306fb8cebc80e3a1dc51dcc31d3eb09691
KulyashDahiya <kulyashdahiya77@gmail.com>
2021-09-19 23:12:54 +0530

    ArrayList

---
+++ Java/Eclipse/JavaCollections/src/collectionArray/LearnArrayList.java
@@ -0,0 +1,20 @@
+package collectionArray;
+
+import java.util.ArrayList;
+
+public class LearnArrayList {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String[] studentsName = new String[31];
+//		studentsName[30] = "KulyashDahiya";
+//		System.out.println(studentsName[30]);
+		
+		ArrayList<String> studentsName = new ArrayList<>();
+		studentsName.add("Yash");
+		
+		
+		
+	}
+
+}
