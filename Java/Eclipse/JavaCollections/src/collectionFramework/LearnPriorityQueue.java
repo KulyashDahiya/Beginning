a4b18fe54107255378498e2a23a0fd9d464e5814
KulyashDahiya <kulyashdahiya77@gmail.com>
2021-09-26 08:17:18 -0700

    Learned Priority Queue

--- Java/Eclipse/JavaCollections/src/collectionFramework/LearnPriorityQueue.java
+++ Java/Eclipse/JavaCollections/src/collectionFramework/LearnPriorityQueue.java
@@ -3,6 +3,7 @@ package collectionFramework;
 import java.util.*;
 
 public class LearnPriorityQueue {
+	// PRIORITY QUEUE USES MIN HEAP AS DEFAULT
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -17,10 +18,28 @@ public class LearnPriorityQueue {
 		// will print with min Heap
 		System.out.println(pq);
 
+		System.out.println(pq.peek());
+
 		// will remove element which has most priority
 		pq.poll(); // in this case the element is the smallest one
 
 		System.out.println(pq);
+
+		// FOR MAXIMUM ELEMENT WE USE COMPARATOR
+
+		System.out.println("Reversing the Priority Queue\n for MAX HEAP");
+		PriorityQueue<Integer> pq1 = new PriorityQueue<>(Comparator.reverseOrder());
+
+		pq1.offer(40);
+		pq1.offer(12);
+		pq1.offer(24);
+		pq1.offer(36);
+
+		System.out.println(pq1);
+
+		System.out.println(pq1.peek());
+		pq1.poll();
+		System.out.println(pq1);
 
 	}
 
