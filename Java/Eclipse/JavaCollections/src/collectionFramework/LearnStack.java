b721e445dcaabe459445a40ec510985c229ec689
KulyashDahiya <kulyashdahiya77@gmail.com>
2021-09-26 18:40:02 +0530

    Learned Stack

--- Java/Eclipse/JavaCollections/src/collectionFramework/LearnStack.java
+++ Java/Eclipse/JavaCollections/src/collectionFramework/LearnStack.java
@@ -1,9 +1,21 @@
 package collectionFramework;
 
+import java.util.*;
+
 public class LearnStack {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Stack<String> animals = new Stack<>();
+		
+		animals.push("Lion");
+		animals.push("Dog");
+		animals.push("Cat");
+		
+		System.out.println("Animals: " + animals);
+		System.out.println(animals.peek());
+		animals.pop();
+		System.out.println(animals.peek());
 
 	}
 
