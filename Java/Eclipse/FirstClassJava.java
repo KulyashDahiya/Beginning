6c7c7e46729f350a1b1a606bfcbd80d45c055946
KulyashDahiya <kulyashdahiya77@gmail.com>
2022-04-19 11:57:26 +0530

    new

---
+++ Java/Eclipse/FirstClassJava.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class FirstClassJava {
+    public static void main(String[] args){
+        System.out.println("Anuj");
+        Scanner sc = new Scanner(System.in);
+    }
+}
