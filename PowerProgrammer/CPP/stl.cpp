c91e0bb08eeb1de6d3f5c765c9e96afba9bdb4ac
KulyashDahiya <kulyashdahiya77@gmail.com>
2022-06-15 00:42:24 +0530

    vector

--- PowerProgrammer/CPP/stl.cpp
+++ PowerProgrammer/CPP/stl.cpp
@@ -11,6 +11,25 @@ void explainPari(){
     cout << arr[1].second;
 }
 
+void explainVector() {
+    vector<int> v;
+
+    v.push_back(1);
+    v.emplace_back(2);
+
+    vector<pair<int, int>>vec;
+
+    vec.push_back({1,2});
+    vec.emplace_back(1,2);
+
+    vector<int> v1(5, 100); //container of 5 instances(100,100,100,100,100)
+    vector<int> v2(5);
+
+    vector<int> v3(5,20);
+    vector<int> v4(v3);
+    
+}
+
 int main(){
     cout<< "Hello World" <<endl ;
     return 0;
