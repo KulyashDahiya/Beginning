8aa09d0b0ab5773bc799bb5b0fd8d7dffbe62408
KulyashDahiya <kulyashdahiya77@gmail.com>
2022-06-16 00:15:40 +0530

    Set and Unordered Set

--- PowerProgrammer/CPP/stl.cpp
+++ PowerProgrammer/CPP/stl.cpp
@@ -181,6 +181,74 @@ void explainPQ() {   //Max Heap
 
 }
 
+void explainSet() {  //Everthing happens in Logarithmic time complexity
+    set<int>st;
+    st.insert(1); //{1}
+    st.emplace(2); //{1,2}
+    st.insert(2); //{1,2}
+    st.insert(4); //{1,2,4}
+    st.insert(3); //{1,2,3,4}
+
+    //Functionality of insert in vector
+    //can be used also, that only increases efficiency
+
+    //begin(), end(), rbegin(), rend(), size(),
+    //empty() and swap() are same as those of above
+
+    //{1,2,3,4,5}
+    auto it = st.find(3);
+
+    //{1,2,3,4,5}
+    auto it = st.find(6); //returns se.end() if element is not found
+    
+    //{1,4,5}
+    st.erase(5); //erases 5 // takes loagarithmic time
+
+    int cnt = st.count(1);
+    
+    auto it = st.find(3);
+    st.erase(it); //it takes constant time
+
+    //{1,2,3,4,5}
+    auto it1 = st.find(2);
+    auto it2 = st.find(4);
+    st.erase(it1, it2); // after erase{1,4,5} [first, last]
+
+    //lower_bound() and upper_bound() function works in the same way
+    //as in vector it does
+
+    //This is the syntax
+    auto it = it.lower_bound(2);
+    auto it = it.upper_bound(3);
+
+}
+
+void explainMultiSet() {
+    //Everthing is same as set
+    //Only stores duplicate elements also
+
+    multiset<int>ms;
+    ms.insert(1) //{1}
+    ms.insert(1) //{1,1}
+    ms.insert(1) //{1,1,1}
+
+    ms.erase(1); //all 1's erased
+
+    int cnt = ms.count(1);
+
+    //only a single one erased
+    ms.erase(ms.find(1));
+
+    ms.erase(ms.find(1), ms.find(1)+2); //find 1 and go till 2
+
+    //rest all functions same as set
+}
+
+void explainUnOrderedSet() {
+    unordered_set<int> st;        //unsorted but unique -- Most cases O(n)
+    //upper_bound and lower_bound function do not work, rest functions are same.
+}
+
 int main(){
     cout<< "Hello World" <<endl ;
     return 0;
