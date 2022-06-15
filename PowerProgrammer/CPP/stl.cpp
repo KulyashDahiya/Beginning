0660e354fa9ee95541e585f44473fa78937ded47
KulyashDahiya <kulyashdahiya77@gmail.com>
2022-06-15 17:29:15 +0530

    Priority Queue and Minimum Heap

--- PowerProgrammer/CPP/stl.cpp
+++ PowerProgrammer/CPP/stl.cpp
@@ -154,6 +154,31 @@ void explainQueue() {  //FIFO
     //size swap empty same as stack
 }
 
+void explainPQ() { 
+    queue<int>  pq;
+    pq.push(5); //{5}
+    pq.push(2); //{5,2}
+    pq.push(8); //{8,5,2}
+    pq.emplace(10); //{10,8,5,2}
+
+    cout << pq.top(); //prints 10
+
+    pq.pop(); //{8,5,2}
+    cout << pq.top(); //prints 8
+
+    //size swap empty fuction same as stack
+
+    //Minimum Heap
+    priority_queue<int, vectot<int>, greater<int>> pq;
+    pq.push(5); //{5}
+    pq.push(2); //{2,5}
+    pq.push(8); //{2,5,8}
+    pq.emplace(10); //{2,5,8,10}
+    
+    cout << pq.pop(); // prints 2
+
+}
+
 int main(){
     cout<< "Hello World" <<endl ;
     return 0;
