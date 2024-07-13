c5da60d1249735d601ff5541a7f43aa85373e773
Kulyash Dahiya <e-kdahiy@pvhcorp.com>
2024-07-13 16:32:18 +0530

    update

--- Pycharm/Random/BinarySearch.py
+++ Pycharm/Random/BinarySearch.py
@@ -13,11 +13,30 @@ def BinarySearch(arr, x):
     
     return "Element Not Found in Given Array"
 
+def RecursiveBinarySearch(arr, x, l, u):
+    mid = (l+u) // 2
+
+    if x == arr[mid] :
+        return mid
+    elif x < arr[mid] :
+        return RecursiveBinarySearch(arr, x, l, mid - 1)
+    elif x > arr[mid] :
+        return RecursiveBinarySearch(arr, x, mid + 1, u)
+    else :
+        return -1
+    
+
 
 
 if __name__ == "__main__":
     arr = [6,23,56,1,465,7,43,234,55,22,12]
+    n = len(arr)
     x = 465
     arr.sort()
     print(f"Given array Sorted : {arr}")
-    print(BinarySearch(arr, x))
+    # print(BinarySearch(arr, x))
+
+    print(RecursiveBinarySearch(arr, x, 0, n-1))
+
+
+
