67517daaa1fbbe0f0271d2648df0eb0a360fe4de
Kulyash Dahiya <kulyashdahiya77@gmail.com>
2024-09-08 12:44:38 +0530

    LeetCode75

--- Editors/LeetCode75/2215.find-the-difference-of-two-arrays.py
+++ Editors/LeetCode75/2215.find-the-difference-of-two-arrays.py
@@ -7,6 +7,11 @@
 # @lc code=start
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        return [list(nums1-nums2), list(nums2-nums1)]
+
         
 # @lc code=end
 
