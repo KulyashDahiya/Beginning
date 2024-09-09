e7f19863ee164ea92aa27a4d1a9db5826dedd1a6
Kulyash Dahiya <kulyashdahiya77@gmail.com>
2024-09-09 14:47:03 +0530

    LeetCode75

--- Editors/LeetCode75/2130.maximum-twin-sum-of-a-linked-list.py
+++ Editors/LeetCode75/2130.maximum-twin-sum-of-a-linked-list.py
@@ -10,8 +10,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+#Amazon
+
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
+
+        slow, fast = head, head
+        maxVal = 0
+
+	    # Get middle of linked list
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # Reverse second part of linked list
+        curr, prev = slow, None
+        while curr:       
+            curr.next, prev, curr = prev, curr, curr.next  
+
+        # printLL(head)
+        # printLL(rev_head)
+
+        while prev:
+            maxVal = max(maxVal, head.val + prev.val)
+            prev = prev.next
+            head = head.next
+
+        return maxVal
         
 # @lc code=end
 
