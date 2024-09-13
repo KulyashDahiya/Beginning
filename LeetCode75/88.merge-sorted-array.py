c41e08e6180b6bbf15cd0d1932a3a6e7800fb76d
Kulyash Dahiya <kulyashdahiya77@gmail.com>
2024-09-13 21:15:17 +0530

    LeetCode75

--- LeetCode75/88.merge-sorted-array.py
+++ LeetCode75/88.merge-sorted-array.py
@@ -10,6 +10,30 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        i = m-1
+        j = n-1
+
+        last = m + n - 1
+
+        while i >= 0 and j >= 0:
+            
+            if nums1[i] > nums2[j]:
+                nums1[last] = nums1[i]
+                i -= 1
+       
+            else:
+                nums1[last] = nums2[j]
+                j -= 1
+        
+            last -= 1
+
+        while j >= 0:
+            nums1[last] = nums2[j]
+            j -= 1
+            last -= 1
+
+
+
         
 # @lc code=end
 
